6dcfe46485ffa636a9d3694832c777c4e5f7368e
changmin <developerQuo@gmail.com>
2023-06-04 19:49:05 +0900

    life cycle test

--- lib/counter.dart
+++ lib/counter.dart
@@ -8,6 +8,14 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,13 +26,19 @@ class _CounterState extends State<Counter> {
             ),
           ),
         ),
-        home: const Scaffold(
-          backgroundColor: Color(0xFFF4EDDB),
+        home: Scaffold(
+          backgroundColor: const Color(0xFFF4EDDB),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                MyLargeTitle(),
+                showTitle ? const MyLargeTitle() : const Text('nothing'),
+                IconButton(
+                  onPressed: toggleTitle,
+                  icon: const Icon(
+                    Icons.remove_red_eye_outlined,
+                  ),
+                )
               ],
             ),
           ),
@@ -32,13 +46,36 @@ class _CounterState extends State<Counter> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  // build 전에 호출
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  // event 발생시?
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  // build는 UI
+  @override
   Widget build(BuildContext context) {
+    print('build');
     return Text(
       'My Large Title',
       style: TextStyle(
