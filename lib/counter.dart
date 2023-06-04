27990a5f46f0259aed584d0e09534fe8370733d6
changmin <developerQuo@gmail.com>
2023-06-04 19:31:26 +0900

    build context

--- lib/counter.dart
+++ lib/counter.dart
@@ -8,38 +8,43 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: const Color(0xFFF4EDDB),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Click Count',
-              style: TextStyle(
-                fontSize: 30,
-              ),
+        theme: ThemeData(
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(
+              color: Colors.red,
             ),
-            for (var n in numbers) Text('$n'),
-            IconButton(
-              onPressed: onClicked,
-              icon: const Icon(Icons.add_box_rounded),
-              iconSize: 40,
-            ),
-          ],
+          ),
         ),
+        home: const Scaffold(
+          backgroundColor: Color(0xFFF4EDDB),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyLargeTitle(),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
-    ));
+    );
   }
 }
