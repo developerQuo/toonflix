d8943253679ba49b2e4d3b221249c12704abf63c
changmin <developerQuo@gmail.com>
2023-06-15 19:54:36 +0900

    get api+

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodayToons() async {
+  Future<List<WebtoonModel>> getTodayToons() async {
     var client = http.Client();
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await client.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        var instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
+      }
+      return webtoonInstances;
     }
     throw Exception('Failed to load data');
   }
